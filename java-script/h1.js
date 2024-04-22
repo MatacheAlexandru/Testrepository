e69fb804f3c0dd3eff9f7d89c0814d0dc74e03df
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-04-22 22:21:48 +0300

    cv-commit

--- java-script/h1.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("menuButton").addEventListener("click", function() {
-  var dropdown = document.getElementById("dropdownContent");
-  dropdown.classList.toggle("show");
-  
-  var icon = document.getElementById("menuIcon");
-  icon.classList.toggle("rotate");
-});
